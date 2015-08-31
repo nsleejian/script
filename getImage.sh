41e04f4ae462cdb95eea0fb52172034eb5c014be
cocoalee <cocoalee@163.com>
2015-08-31 23:28:01 +0800

    循环抓取图片脚本

--- getImage.sh
+++ getImage.sh
@@ -1,12 +1,5 @@
-# script
-一些脚本
-
-
-###循环抓取图片
-```
-
-
 #!/bin/bash
+
 for i in $(seq 1 24 )
 
 do 
@@ -15,5 +8,3 @@ curl -O https://coding.net/static/project_icon/scenery-${i}.png
 curl -O https://coding.net/static/fruit_avatar/Fruit-${i}.png
 
 done
-
-```
